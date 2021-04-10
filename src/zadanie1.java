fa412170f0a733dd5f9dce129bd684c3eb8ed1cb
Marcin-Krystkowiak <marcin.krystkowiak@o2.pl>
2021-04-10 21:16:10 +0200

    Stworzenie projektu

--- src/zadanie1.java
+++ src/zadanie1.java
@@ -1,5 +1,6 @@
 public class zadanie1 {
     public static void main(String[] args) {
-System.out.println("Uczę \n się \n programowania");
-    //hujhujh}
+        System.out.println("Uczę \n się \n programowania");
+        //
+    }
 }
